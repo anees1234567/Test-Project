193e5d54df303b009f62c00d6453689a1083d336
Anees <Anees@pappyjoe.com>
2025-05-24 12:10:13 +0530

    Add Autocomplete component with debounce functionality and integrate into Home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Autocomplete from '../../AutoComplete/Autocomplete'
 
 const Home = () => {
   return (
-    <div>Home</div>
+ <>
+      <div>Home</div>
+      <Autocomplete/>
+ </>
   )
 }
 
--- src/Routes/GlobalRouter.tsx
+++ src/Routes/GlobalRouter.tsx
@@ -2,11 +2,18 @@ import { createBrowserRouter, Navigate, RouteObject  } from "react-router-dom";
 import React from "react";
 import PageNotFound from "../ErrorPages/PageNotFound";
 import Home from "../Pages/Home/Home";
-
+const ErrorElement = ()=>{
+     return <div className="flex flex-col justify-center items-center h-screen"> 
+        <h1 className="text-4xl font-bold text-red-500">Error</h1>
+        <p className="text-lg text-gray-700">Something went wrong!</p>
+        <p className="text-lg text-gray-700">Please try again later.</p>
+     </div>
+}
 
 const routelist:RouteObject[]=[
     {
         path:"",
+        errorElement:<ErrorElement/>,
         element:<Home/>
     },
     {
